a834db2639cfcef35e54b4ba7a7fb2fdb194d130
milemotigo <mtigo@gmail.com>
2023-07-05 03:04:04 +0100

    task 0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,51 +1,25 @@
-#!/usr/bin/env python3
-import yaml
+# Installs a Nginx server with custom HTTP header that will be appllicable for 1st and 2nd server
 
-bash_script = """
-#!/usr/bin/env bash
-# This script configures Nginx with a custom HTTP response header on a new Ubuntu machine
-
-# Restart Nginx
-sudo service nginx restart
-"""
-
-manifest = {
-    "manifest.yml": [
-        {
-            "name": "Configure Nginx with custom HTTP response header",
-            "tasks": [
-                {
-                    "name": "Update system packages",
-                    "command": "apt-get update",
-                    "become": True
-                },
-                {
-                    "name": "Upgrade system packages",
-                    "command": "apt-get upgrade -y",
-                    "become": True
-                },
-                {
-                    "name": "Install Nginx",
-                    "command": "apt-get install -y nginx",
-                    "become": True
-                },
-                {
-                    "name": "Configure custom HTTP response header",
-                    "command": "echo 'add_header X-Served-By $hostname;' | tee /etc/nginx/conf.d/custom_header.conf",
-                    "become": True
-                },
-                {
-                    "name": "Restart Nginx",
-                    "service": {
-                        "name": "nginx",
-                        "state": "restarted"
-                    },
-                    "become": True
-                }
-            ]
-        }
-    ]
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-with open("manifest.yml", "w") as f:
-    yaml.dump(manifest, f)
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}

54bf4dd9b567d4c9546f0829fae1f5447dc096e6
milemotigo <mtigo@gmail.com>
2023-07-25 17:05:30 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''returns information about his/her TODO list progress.
+'''
+import json
+import requests
+import sys
+
+
+def resp(uid):
+    employee = None
+    todo = 0
+    taskcom = 0
+    task_completed = []
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    for user in users:
+        if user["id"] == uid:
+            employee = user["name"]
+
+    for task in tasks:
+        if task['userId'] == uid:
+            todo += 1
+            if task['completed']:
+                taskcom += 1
+                task_completed.append(task['title'])
+    emp_details = f'Employee {employee} is done with tasks({taskcom}/{todo})'
+    print(emp_details)
+    for title in task_completed:
+        print("\t {}".format(title))
+
+
+if __name__ == '__main__':
+    uid = int(sys.argv[1])
+    resp(uid)

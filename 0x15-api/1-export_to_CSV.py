856bc1fee18f4eeba860cb7159993b5d41a51526
milemotigo <mtigo@gmail.com>
2023-07-25 22:47:52 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-'''
-A script to export data in the CSV format.
-'''
-
+"""
+Using a REST API, for a given employee ID and returns information
+about his/her TODO list progress.
+"""
 import csv
 import requests
 import sys
 
 
 def export_to_csv(uid):
-    '''
-    export to csv
-    '''
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        uid)
-    todo = requests.get(url, verify=False).json()
-    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
-            taskwriter.writerow([uid, user.get('username'),
-                                 task.get('completed'),
-                                 task.get('title')])
+    '''method to csv'''
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+
+    csv_file = sys.argv[1] + '.csv'
+
+    with open(csv_file, mode='w') as export_csv_file:
+        file_to_export = csv.writer(
+            export_csv_file, delimiter=',',
+            quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for user in users:
+            if uid == user.get('id'):
+                USERNAME = user.get('username')
+
+        for task in todos:
+            USER_ID = task.get('userId')
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            if uid == task.get('userId'):
+                file_to_export.writerow([
+                     USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
 
 
 if __name__ == '__main__':
-    '''main function'''
     uid = int(sys.argv[1])
     export_to_csv(uid)

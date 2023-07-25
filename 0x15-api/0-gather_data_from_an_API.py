4bde11c8e73aadfd8028b04a6d2ba86faef09fb6
milemotigo <mtigo@gmail.com>
2023-07-25 17:13:37 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,7 +24,7 @@ def resp(uid):
             if task['completed']:
                 taskcom += 1
                 task_completed.append(task['title'])
-    emp_details = f'Employee {employee} is done with tasks({taskcom}/{todo})'
+    emp_details = 'Employee {} is done with tasks({}/{})'.format(employee, taskcom, todo)
     print(emp_details)
     for title in task_completed:
         print("\t {}".format(title))

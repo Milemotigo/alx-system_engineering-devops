ed29b16ac47e866f819c3b130ee7f1cb9fb13e19
milemotigo <mtigo@gmail.com>
2023-07-26 11:28:48 +0100

    fix1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,27 @@
-#!/usr/bin/python3
 '''Python script to export data in the CSV format.'''
-
 import csv
 import requests
 import sys
 
 
 def import_csv(uid):
-    task = "https://jsonplaceholder.typicode.com/todos"
-    usr = "https://jsonplaceholder.typicode.com/users"
+    task_url = "https://jsonplaceholder.typicode.com/todos"
+    usr_url = "https://jsonplaceholder.typicode.com/users"
 
-    todos = requests.get(task).json()
-    users = requests.get(usr).json()
+    todos = requests.get(task_url).json()
+    users = requests.get(usr_url).json()
 
     name = None
     for user in users:
         if user['id'] == uid:
-            name = user['name']
+            employee = user['username']
 
-        with open("{}.csv".format(uid), "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            for todo in todos:
-                if todo['userId'] == uid:
-                    writer.writerow(
-                        [uid, name, todo.get("completed"), todo.get("title")])
+    with open("{}.csv".format(uid), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            if todo['userId'] == uid:
+                writer.writerow([uid, employee,
+                                 todo.get("completed"), todo.get("title")])
 
 
 if __name__ == '__main__':

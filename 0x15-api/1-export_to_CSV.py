e9ff0e410c2c21968009315b2ea815abb4fc8597
milemotigo <mtigo@gmail.com>
2023-07-26 11:02:09 +0100

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,24 @@
-#!/usr/bin/python3
-"""
-Using a REST API, for a given employee ID and returns information
-about his/her TODO list progress.
-"""
 import csv
 import requests
 import sys
 
 
-def export_to_csv(uid):
-    '''method to csv'''
-    todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos').json()
-    users = requests.get(
-        'https://jsonplaceholder.typicode.com/users').json()
+def export_to_csv(user_id):
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    response = requests.get(url)
 
-    csv_file = sys.argv[1] + '.csv'
+    todo = response.json()
 
-    with open(csv_file, mode='w') as export_csv_file:
-        file_to_export = csv.writer(
-            export_csv_file, delimiter=',',
-            quotechar='"', quoting=csv.QUOTE_ALL)
-
-        for user in users:
-            if uid == user.get('id'):
-                USERNAME = user.get('username')
-
-        for task in todos:
-            USER_ID = task.get('userId')
-            TASK_COMPLETED_STATUS = task.get('completed')
-            TASK_TITLE = task.get('title')
-            if uid == task.get('userId'):
-                file_to_export.writerow([
-                     USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['userId', 'id', 'title', 'completed']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for task in todo:
+            writer.writerow(task)
 
 
 if __name__ == '__main__':
-    uid = int(sys.argv[1])
-    export_to_csv(uid)
+    '''main'''
+    user_id = sys.argv[1]
+    export_to_csv(user_id)

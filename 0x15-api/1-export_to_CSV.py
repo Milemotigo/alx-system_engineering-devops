3c45d00b07682452eb45e99f756801d3bbc61ed4
milemotigo <mtigo@gmail.com>
2023-07-26 11:54:04 +0100

    finall

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,8 +5,9 @@ import requests
 import sys
 
 
-def import_csv(uid):
-    '''import function'''
+if __name__ == '__main__':
+
+    uid = int(sys.argv[1])
     task_url = "https://jsonplaceholder.typicode.com/todos"
     usr_url = "https://jsonplaceholder.typicode.com/users"
 
@@ -18,17 +19,11 @@ def import_csv(uid):
     for user in users:
         if user['id'] == uid:
             empl = user['username']
-
-    with open("{}.csv".format(uid), "w", newline="") as csvfile:
+    filename = "{}.csv".format(uid)
+    with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for t in todos:
             if t['userId'] == uid:
                 completed = t.get("completed")
                 title = t.get("title")
                 writer.writerow([uid, empl, completed, title])
-
-
-if __name__ == '__main__':
-    '''main'''
-    uid = int(sys.argv[1])
-    import_csv(uid)

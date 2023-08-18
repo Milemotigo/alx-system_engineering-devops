14bd99d00c663246a44ac21747fd14e2ece99878
milemotigo <mtigo@gmail.com>
2023-08-18 11:38:15 +0000

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
-# Sky is the limit, let's bring that limit higher
-exec { 'find-n-replace':
-  command => "sed -i 's/ULIMIT = -n 15/ULIMIT = -n 5000/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => $::path
+# Increases the ulimit in nginx
+exec {'findNreplace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }

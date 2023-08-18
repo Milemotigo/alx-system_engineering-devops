71a90e062c871398fc8db46eac1e72770d82aff1
milemotigo <mtigo@gmail.com>
2023-08-18 10:46:40 +0000

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' etc/nginx/nginx.conf; sudo service nginx restart",
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => $::path
 }
